b8b7425fbf0b56bbcb3c81308ec3d5dd59cdd5f6
Ethan Nguyen <thanhnx.605@gmail.com>
2019-08-13 20:48:19 +0700

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': ['error', { 'trailingComma': 'es5', 'tabWidth': 2, 'singleQuote': true, 'printWidth': 120 }],
+  }
+};
