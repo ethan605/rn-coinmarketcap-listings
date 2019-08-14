8ca2cab4218a6aa62ab172affd30528defa93037
Ethan Nguyen <thanhnx.605@gmail.com>
2019-08-14 19:51:00 +0700

    Refactor globals.js -> globals.tsx

--- __tests__/components/App.spec.tsx
+++ __tests__/components/App.spec.tsx
@@ -6,7 +6,7 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import App from '../App';
+import App from '../../App';
 
 describe('App', (): void => {
   it('renders correctly', (): void => {
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  setupFiles: ['<rootDir>/__mocks__/globals.js'],
   coverageDirectory: '<rootDir>/__tests__/coverage',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   modulePathIgnorePatterns: ['<rootDir>/__tests__/coverage'],
   preset: 'react-native',
+  setupFiles: ['<rootDir>/__mocks__/globals.tsx'],
   testEnvironment: 'node',
 };
