47bf480c8b7e751d802e21cca91bfb55875e727a
Ethan Nguyen <thanhnx.605@gmail.com>
2019-08-16 21:39:01 +0700

    Refine jest setups

--- __mocks__/globals.tsx
+++ __mocks__/globals.tsx
@@ -4,5 +4,4 @@ import 'reflect-metadata';
 console.debug = jest.fn();
 console.error = jest.fn();
 console.warn = jest.fn();
-// Date.now = jest.fn(() => 1514739600000); // Mon Jan 01 2018 00:00:00 GMT+0700
 jest.useFakeTimers();
